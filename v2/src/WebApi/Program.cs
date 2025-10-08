5847c9c2996cd758c94bb94b3ccbf570539d347e
pedrofrohmut <pedrofrohmut@gmail.com>
2025-10-08 15:01:47 -0300

    editorconfig

--- v2/src/WebApi/Program.cs
+++ v2/src/WebApi/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// Add Dbconnection, cors and controllers
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+// Configure: UseCors and MapControllers
+// Middlewares: TimeIt, ExceptionHandler, Authorization, DatabaseConnection
 
 app.Run();

e67430eb797bbf24e9b2eb7a9aa73569b217791b
pedrofrohmut <pedrofrohmut@gmail.com>
2025-10-23 17:33:49 -0300

    Refactor controller

--- v2/src/WebApi/Controllers/UsersController.cs
+++ v2/src/WebApi/Controllers/UsersController.cs
@@ -25,66 +25,72 @@ public class UsersController : ControllerBase
     [HttpPost("signup")]
     public async Task SignUp(SignUpBody body)
     {
+        var useCase = UseCasesFactory.GetUserSignUpUseCase();
+        var input = new UserSignUpInput {
+            Name = body.Name,
+            Email = body.Email,
+            Password = body.Password,
+        };
+
+        Result<UserSignUpOutput> result;
         try {
-            var useCase = UseCasesFactory.GetUserSignUpUseCase();
-            var input = new UserSignUpInput {
-                Name = body.Name,
-                Email = body.Email,
-                Password = body.Password,
-            };
-            var result = await useCase.Execute(input);
-
-            if (result.IsSuccess) {
-                HttpContext.Response.StatusCode = 201;
-                await HttpContext.Response.WriteAsync("Sign Up: User created successfully.");
-                return;
-            }
-
-            if (result.Error is InvalidUserError || result.Error is EmailAlreadyTakenError) {
-                HttpContext.Response.StatusCode = 400;
-                await HttpContext.Response.WriteAsync(result.Error.Message);
-                return;
-            }
-
-            await ControllerUtils.WriteErrorNotMappedResponse(HttpContext);
+            result = await useCase.Execute(input);
         } catch (Exception e) {
             await ControllerUtils.WriteExceptionResponse(HttpContext, e);
+            return;
         }
+
+        if (result.IsSuccess) {
+            HttpContext.Response.StatusCode = 201;
+            await HttpContext.Response.WriteAsync("Sign Up: User created successfully.");
+            return;
+        }
+
+        if (result.Error is InvalidUserError || result.Error is EmailAlreadyTakenError) {
+            HttpContext.Response.StatusCode = 400;
+            await HttpContext.Response.WriteAsync(result.Error.Message);
+            return;
+        }
+
+        await ControllerUtils.WriteErrorNotMappedResponse(HttpContext);
     }
 
     [HttpPost("signin")]
     public async Task SignIn(SignInBody body)
     {
+        var useCase = UseCasesFactory.GetUserSignInUseCase();
+        var input = new UserSignInInput {
+            Email = body.Email,
+            Password = body.Password,
+        };
+
+        Result<UserSignInOutput> result;
         try {
-            var useCase = UseCasesFactory.GetUserSignInUseCase();
-            var input = new UserSignInInput {
-                Email = body.Email,
-                Password = body.Password,
-            };
-            var result = await useCase.Execute(input);
-
-            if (result.IsSuccess) {
-                HttpContext.Response.StatusCode = 200;
-                await HttpContext.Response.WriteAsJsonAsync(result.Payload);
-                return;
-            }
-
-            if (result.Error is InvalidUserError || result.Error is PasswordMatchError) {
-                HttpContext.Response.StatusCode = 400;
-                await HttpContext.Response.WriteAsync(result.Error.Message);
-                return;
-            }
-
-            if (result.Error is UserNotFoundError) {
-                HttpContext.Response.StatusCode = 404;
-                await HttpContext.Response.WriteAsync(result.Error.Message);
-                return;
-            }
-
-            await ControllerUtils.WriteErrorNotMappedResponse(HttpContext);
+            result = await useCase.Execute(input);
         } catch (Exception e) {
             await ControllerUtils.WriteExceptionResponse(HttpContext, e);
+            return;
         }
+
+        if (result.IsSuccess) {
+            HttpContext.Response.StatusCode = 200;
+            await HttpContext.Response.WriteAsJsonAsync(result.Payload);
+            return;
+        }
+
+        if (result.Error is InvalidUserError || result.Error is PasswordMatchError) {
+            HttpContext.Response.StatusCode = 400;
+            await HttpContext.Response.WriteAsync(result.Error.Message);
+            return;
+        }
+
+        if (result.Error is UserNotFoundError) {
+            HttpContext.Response.StatusCode = 404;
+            await HttpContext.Response.WriteAsync(result.Error.Message);
+            return;
+        }
+
+        await ControllerUtils.WriteErrorNotMappedResponse(HttpContext);
     }
 
     /* Just to check if the Authentication token is valid or not */

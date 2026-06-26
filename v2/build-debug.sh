9ae320722d94db9f4fd9aa3573e5cbec62b8ec1c
pedrofrohmut <pedrofrohmut@gmail.com>
2026-06-26 12:12:25 -0300

    Run option to build script

--- v2/build-debug.sh
+++ v2/build-debug.sh
@@ -1,3 +1,12 @@
 #! /usr/bin/env bash
 
 dotnet build -c Debug -p:NoWarn=CS1998
+
+if ! [[ $? -eq 0 ]]; then
+    echo "[ERROR] Build failed. Exiting..."
+    exit 1
+fi
+
+if [[ $* == *--run* ]]; then
+    dotnet run --project ./src/WebApi/WebApi.csproj
+fi
